e4e7c917f6c6b11e7e43b66f3b36a64ead1d5870
rasmusselsmark <rasmus@selsmark.dk>
2021-05-22 21:34:35 +0200

    Clean up CameraFollow script

--- SelfDrivingCar/Assets/Scripts/CameraFollow.cs
+++ SelfDrivingCar/Assets/Scripts/CameraFollow.cs
@@ -2,21 +2,21 @@
 
 public class CameraFollow : MonoBehaviour
 {
-	// the game object camera should look at
+	// the game object camera is looking at
 	public Transform Target;
 
 	// Camera offset from the target position
 	public Vector3 CameraOffset;
 
-	// optionally look above target  
+	// optionally look e.g. above target
 	public Vector3 TargetOffset;
 
 	// Camera speeds
 	[Range(0, 10)]
-	public float lerpPositionMultiplier = 1f;
+	public float LerpPositionMultiplier = 1f;
 
 	[Range(0, 10)]
-	public float lerpRotationMultiplier = 1f;
+	public float LerpRotationMultiplier = 1f;
 
 	// We use a rigidbody to prevent the camera from going in walls but it means sometime it can get stuck
 	Rigidbody rb;
@@ -29,7 +29,7 @@ public class CameraFollow : MonoBehaviour
 	void FixedUpdate()
 	{
 		// normalise velocity so it doesn't jump too far
-		this.rb.velocity.Normalize();
+		rb.velocity.Normalize();
 
 		// Save transform locally
 		Quaternion curRot = transform.rotation;
@@ -38,20 +38,8 @@ public class CameraFollow : MonoBehaviour
 		// Look at the target
 		transform.LookAt(Target.position + TargetOffset);
 
-		// // Keep the camera above the target y position
-		// if (tPos.y < target.position.y)
-		// {
-		// 	tPos.y = target.position.y;
-		// }
-
 		// Set transform with lerp
-		transform.position = Vector3.Lerp(transform.position, tPos, Time.fixedDeltaTime * lerpPositionMultiplier);
-		transform.rotation = Quaternion.Lerp(curRot, transform.rotation, Time.fixedDeltaTime * lerpRotationMultiplier);
-
-		// Keep camera above the y:0.5f to prevent camera going underground
-		// if (transform.position.y < 0.5f)
-		// {
-		// 	transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
-		// }
+		transform.position = Vector3.Lerp(transform.position, tPos, Time.fixedDeltaTime * LerpPositionMultiplier);
+		transform.rotation = Quaternion.Lerp(curRot, transform.rotation, Time.fixedDeltaTime * LerpRotationMultiplier);
 	}
 }
